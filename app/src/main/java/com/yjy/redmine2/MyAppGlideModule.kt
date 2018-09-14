3e1a0bed547a03c8a5959e3824bc9e25d7a8067c
yuanjunyao <yuanjunyao@jbangit.com>
2018-09-14 21:53:59 +0800

    引入Glide

---
+++ app/src/main/java/com/yjy/redmine2/MyAppGlideModule.kt
@@ -0,0 +1,18 @@
+package com.yjy.redmine2
+
+import android.content.Context
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
+
+@GlideModule
+class MyAppGlideModule : AppGlideModule() {
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        super.applyOptions(context, builder)
+        builder.setDefaultRequestOptions(object : RequestOptions(){
+
+        })
+    }
+}
